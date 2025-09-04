bdb593e48dc335700c804c8a144beaa11708cafd
Tuğçe Gezer <tugcegezeeer@gmail.com>
2025-09-04 15:43:58 +0300

    Fourth Commit

--- Business/Concrete/ProductManager.cs
+++ Business/Concrete/ProductManager.cs
@@ -7,6 +7,8 @@ using Business.Abstract;
 using Entities.Concrete;
 using DataAccess.Abstract;
 using DataAccess.Concrete.EntityFramework;
+using Core.Utilities.Results;
+using Business.Contants;
 
 namespace Business.Concrete
 {
@@ -17,35 +19,36 @@ namespace Business.Concrete
         { 
             _productDal = productDal;
         }
-        public void Add(Product product)
+        public IDataResult<Product> GetById(int productId)
+        {
+            return new SuccessDataResult<Product>(_productDal.Get(filter: p => p.ProductId == productId)); 
+        }
+
+        public IDataResult<List<Product>> GetList()
+        {
+            return new SuccessDataResult<List<Product>> ( _productDal.GetList().ToList());
+        }
+
+        public IDataResult<List<Product>> GetListByCategory(int categoryId)
+        {
+            return new SuccessDataResult<List<Product>>(_productDal.GetList(filter: p => p.CategoryId == categoryId).ToList());
+        }
+        public IResult Add(Product product)
         {
             //Business Codes
             _productDal.Add(product);
+            return new SuccessResult(Messages.ProductAdded);
         }
 
-        public void Delete(Product product)
+        public IResult Delete(Product product)
         {
             _productDal.Delete(product);
+            return new SuccessResult(Messages.ProductDeleted);
         }
-
-        public Product GetById(int productId)
-        {
-            return _productDal.Get(filter: p => p.ProductId == productId);
-        }
-
-        public List<Product> GetList()
-        {
-            return _productDal.GetList().ToList();
-        }
-
-        public List<Product> GetListByCategory(int categoryId)
-        {
-            return _productDal.GetList(filter: p => p.CategoryId == categoryId).ToList();
-        }
-
-        public void Update(Product product)
+        public IResult Update(Product product)
         {
             _productDal.Update(product);
+            return new SuccessResult(Messages.ProductUpdated);
         }
     }
 }
---
+++ Core/Utilities/Results/SuccessResult.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Core.Utilities.Results;
+
+namespace Core.Utilities.Results
+{
+    public class SuccessResult:Result
+    {
+        public SuccessResult(string message): base(true,message)
+        {
+
+        }
+        public SuccessResult(): base(success: true)
+        {
+
+        }
+    }
+}
